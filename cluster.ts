2468e93f7be15231793edc9da23794469c9c3aa6
FUJI Goro (gfx) <gfuji@cpan.org>
2017-06-21 22:47:23 +0900

    initial commit

---
+++ cluster.ts
@@ -0,0 +1,24 @@
+import * as cluster from 'cluster';
+import * as  http from 'http';
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < 2; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(8000);
+
+  console.log(`Worker ${process.pid} started`);
+}
